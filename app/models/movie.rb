e1f30f292ee3ee9f2416334c93b7e202771e403a
birchlore <jacksondcunningham@gmail.com>
2015-02-03 23:43:02 +0000

    refactor

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,25 +1,16 @@
 class Movie < ActiveRecord::Base
   has_many :reviews
-
-  validates :title,
-    presence: true
-
-  validates :director,
-    presence: true
-
-  validates :runtime_in_minutes,
-    numericality: { only_integer: true }
-
-  validates :description,
-    presence: true
-
-  validates :poster_image_url,
-    presence: true
-
-  validates :release_date,
-    presence: true
-
+  validates :title, presence: true
+  validates :director, presence: true
+  validates :runtime_in_minutes, numericality: { only_integer: true }
+  validates :description, presence: true
+  validates :poster_image_url, presence: true
+  validates :release_date, presence: true
   validate :release_date_is_in_the_future
+
+  def review_average
+    reviews.average(:rating_out_of_ten) if reviews
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :movies
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :reviews
+  root to: 'movies#index'
+
 end
